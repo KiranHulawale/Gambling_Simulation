839a78bc68981e74fd6f830c507392dcd4759ba4
Kiran <kiranhulawale123@gmail.com>
2022-04-07 17:18:11 +0530

    Added UC-2 to win or loose bet

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -1,15 +1,24 @@
 package com.bridgelabz;
+import java.util.Random;
 
 public class Gambler {
+    static int stack=100;
+    final static int bet=1;
 
     public void game() {
-        System.out.println("Game Starts");
-        int stack=100;
-        int BET=1;
         System.out.println("Every Day Stack: "+stack);
-        System.out.println("Bet Price: "+BET);
-    }
+        System.out.println("Bet Price: "+bet);
 
+        Random random = new Random();
+        int Bet = random.nextInt(2);
+
+        if(Bet == 1) {
+            System.out.println("Bet Win");
+        }else {
+            System.out.println("Bet Loss");
+        }
+
+    }
     public static void main(String[] args) {
         Gambler gambling=new Gambler();
         gambling.game();
