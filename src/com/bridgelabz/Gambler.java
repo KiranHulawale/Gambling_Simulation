59697b7d16017ed294755a7ca327dac545095c34
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-04-07 17:37:00 +0530

    Update Gambler.java

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -2,25 +2,31 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Gambler {
-    static int stack=100;
-    final static int bet=1;
+    static int stack = 100;
+    final static int bet = 1;
 
     public void game() {
-        System.out.println("Every Day Stack: "+stack);
-        System.out.println("Bet Price: "+bet);
+        System.out.println("Every Day Stack: " + stack);
+        System.out.println("Beat Price: " + bet);
 
-        Random random = new Random();
-        int Bet = random.nextInt(2);
+        int stack_Upper = 150;
+        int stack_Lower = 50;
 
-        if(Bet == 1) {
-            System.out.println("Bet Win");
-        }else {
-            System.out.println("Bet Loss");
+        while (stack > stack_Lower && stack < stack_Upper) {
+            Random random = new Random();
+            int Bet = random.nextInt(2);
+            if (Bet == 1) {
+                System.out.println("Bet Win");
+                System.out.println("Final Amount: " + (stack += bet));
+            } else {
+                System.out.println("Bet Loss");
+                System.out.println("Final Amount: " + (stack -= bet));
+            }
         }
-
     }
+
     public static void main(String[] args) {
-        Gambler gambling=new Gambler();
-        gambling.game();
+            Gambler gambling = new Gambler();
+            gambling.game();
     }
 }
