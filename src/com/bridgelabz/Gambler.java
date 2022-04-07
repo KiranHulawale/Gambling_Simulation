95eb98a2779458f6ad83faa40ea1b7b6a211f184
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-04-07 18:11:25 +0530

    Update Gambler.java

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -2,25 +2,46 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Gambler {
-    static int stack=100;
-    final static int bet=1;
+    static int stake = 100;
+    final static int bet = 1;
 
     public void game() {
-        System.out.println("Every Day Stack: "+stack);
-        System.out.println("Bet Price: "+bet);
+        System.out.println("Every Day Stack: " + stake);
+        System.out.println("Bet Price: " + bet);
 
-        Random random = new Random();
-        int Bet = random.nextInt(2);
-
-        if(Bet == 1) {
-            System.out.println("Bet Win");
-        }else {
-            System.out.println("Bet Loss");
+        int amount=0;
+        int won = 0;
+        int lost=0;
+        int day;
+        for (day = 0; day < 20; day++) {
+            while (won > (stake/2)  || lost < (stake/2)) {
+                Random random = new Random();
+                int Bet = random.nextInt(2);
+                if (Bet  == bet) {
+                    won++;
+                    stake++;
+                } else if (Bet == 0) {
+                    lost++;
+                    stake--;
+                }
+            }
+            if (won > (stake/2)) {
+                won++;
+               // amount = amount + 50;
+            } else if (lost < (stake/2)) {
+                lost++;
+               // amount = amount - 50;
+            }
         }
-
+        if (won > lost) {
+            System.out.println("After " + day + " days the total amount won is: " + amount);
+        } else if (lost > won) {
+            System.out.println("After " + day + " days the total amount lost is: " + amount);
+        } else System.out.println("neither win or lost");
     }
+
     public static void main(String[] args) {
-        Gambler gambling=new Gambler();
-        gambling.game();
+            Gambler gambling = new Gambler();
+            gambling.game();
     }
 }
